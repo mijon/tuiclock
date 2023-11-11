af4831f4f52b9e8d70133248fdf402cc87c281c6
Michael Jones <michael.jones.gsy@gmail.com>
2023-11-11 21:51:05 +0000

    displays a big text time

--- src/ui.rs
+++ src/ui.rs
@@ -3,24 +3,30 @@ use ratatui::{
     style::{Color, Style},
     widgets::{Block, BorderType, Borders, Paragraph},
 };
+use tui_big_text::BigText;
 
 use crate::app::App;
 
 pub fn render(app: &mut App, f: &mut Frame) {
     f.render_widget(
-        Paragraph::new(
-            format!("{}", app.render_time())
-    )
-    .block(
-        Block::default()
-            .title("Time")
-            .title_alignment(Alignment::Center)
-            .borders(Borders::ALL)
-            .border_type(BorderType::Rounded),
-        )
-        .style(Style::default().fg(Color::Blue))
-        .alignment(Alignment::Center),
+    //     Paragraph::new(
+    //         format!("{}", app.render_time())
+    // )
+    big_time(app.render_time()),
+    // .block(
+    //     Block::default()
+    //         .title("Time")
+    //         .title_alignment(Alignment::Center)
+    //         .borders(Borders::ALL)
+    //         .border_type(BorderType::Rounded),
+    //     )
+    //     .style(Style::default().fg(Color::Blue))
+    //     .alignment(Alignment::Center),
         f.size()
     )
 }
 
+pub fn big_time(time_string: String) -> BigText<'static> {
+    let lines = vec![time_string.into()];
+    tui_big_text::BigTextBuilder::default().lines(lines).build().unwrap()
+}

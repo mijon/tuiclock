e9badf12ebb1a43da6067a4584dd38949c4abb3f
Michael Jones <michael.jones.gsy@gmail.com>
2023-11-12 15:57:04 +0000

    removes unneeded comments

--- src/ui.rs
+++ src/ui.rs
@@ -9,19 +9,7 @@ use crate::app::App;
 
 pub fn render(app: &mut App, f: &mut Frame) {
     f.render_widget(
-    //     Paragraph::new(
-    //         format!("{}", app.render_time())
-    // )
-    big_time(app.render_time()),
-    // .block(
-    //     Block::default()
-    //         .title("Time")
-    //         .title_alignment(Alignment::Center)
-    //         .borders(Borders::ALL)
-    //         .border_type(BorderType::Rounded),
-    //     )
-    //     .style(Style::default().fg(Color::Blue))
-    //     .alignment(Alignment::Center),
+        big_time(app.render_time()),
         f.size()
     )
 }
